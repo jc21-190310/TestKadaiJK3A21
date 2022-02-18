d856947aaa36b0d3a623f435c95a157625d73228
真 佐藤 <190310@jc-21.jp>
2022-02-18 16:20:15 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class JetCoaster
+    {
+        public static int ride
+            (int k, int[] g, int r)
+        {
+            return 16;
+        }
+    }
+}
